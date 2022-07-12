e2b644475f31514c232929da7d19e884a4929d87
Pratik Tayde <pratik.tayde@lntinfotech.com>
2022-07-13 00:54:40 +0530

    new files

---
+++ Challenge2/GetMetadata.ps1
@@ -0,0 +1,14 @@
+﻿Connect-Azlogin
+
+$storageContext = New-AzStorageContext -StorageAccountName "<AccntName>" -StorageAccountKey "<AccntKey>"
+
+$allContainers = Get-AzStorageContainer -Context $storageContext
+
+
+foreach($container in $allContainers){
+    $metadata = $container.BlobContainerProperties.Metadata
+    $metadata | ConvertTo-Json -Depth 99
+
+    Write-Host "Key1:" $metadata.Key1.value
+}
+
---
+++ Challenge3/GetKey.ps1
@@ -0,0 +1,15 @@
+﻿
+function Get-KeyValue{
+param($empname, $key)
+
+    $Content = $JSON | ConvertFrom-Json
+
+    $Output = foreach( $emp in $Content.$empname.psobject.Properties ) {
+        [PSCustomObject]@{
+
+            keyValue = $key.Valuekey
+        }
+    }
+    $Output | Format-List
+
+}
